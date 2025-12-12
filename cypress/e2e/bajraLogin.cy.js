ecd7191f0e02786f0964f262b020028fe850dc60
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2025-12-12 16:50:52 +0545

    feat: add e2e test for login functionality in bajra's website

---
+++ cypress/e2e/bajraLogin.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe("Bajra Login flow", () => {
+    context("User is on the browser with internet enabled.", () => {
+        it("Logs into bajra webstie", () => {
+            cy.visit("web/login")
+            cy.get("#login").should("be.visible").type(Cypress.env("email"))
+            cy.get("#password").should("be.visible").type(Cypress.env("password"))
+            cy.get("#wrapwrap > main > div > form > div.clearfix.oe_login_buttons.text-center.mb-1.pt-3 > button").click()
+        })
+    })
+})
